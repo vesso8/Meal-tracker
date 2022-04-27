28582cc1ea8508682637010c381f4e6d8f3744b0
vesso8 <skittles8@abv.bg>
2022-04-27 10:20:48 +0300

    Changing Debug=True

--- meal_tracker/settings.py
+++ meal_tracker/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'sk')
-
+DEBUG=True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', '')
 
