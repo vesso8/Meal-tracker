fa968feac761bd07d170342fa373cf006a1ad36f
vesso8 <skittles8@abv.bg>
2022-04-29 22:29:36 +0300

    Editing calorie_counter

--- meal_tracker/meal/views/meal_tracker_views.py
+++ meal_tracker/meal/views/meal_tracker_views.py
@@ -14,20 +14,20 @@ UserModel = get_user_model()
 
 @login_required(login_url=reverse_lazy('login'), redirect_field_name='redirect_to')
 def calorie_counter(request):
-    calories = Meal_tracker.objects.filter(person_of=request.user).last()
-    calorie_goal = calories.calorie_goal
+    last_day = Meal_tracker.objects.filter(person_of=request.user).last()
+    calorie_goal = last_day.calorie_goal
 
-    if date.today() > calories.date:
+    if date.today() > last_day.date:
         profile = Meal_tracker.objects.create(person_of=request.user)
         profile.save()
-    calories = Meal_tracker.objects.filter(person_of=request.user).last()
-    all_food_today = PostFood.objects.filter(profile=calories)
-    calorie_goal_status = calorie_goal - calories.total_calorie
+    last_day = Meal_tracker.objects.filter(person_of=request.user).last()
+    all_food_today = PostFood.objects.filter(profile=last_day)
+    calorie_goal_status = calorie_goal - last_day.total_calorie
     over_calorie = 0
     if calorie_goal_status < 0:
         over_calorie = abs(calorie_goal_status)
     context = {
-        'total_calorie': calories.total_calorie,
+        'total_calorie': last_day.total_calorie,
         'calorie_goal': calorie_goal,
         'calorie_goal_status': calorie_goal_status,
         'over_calorie': over_calorie,
